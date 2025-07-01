726014a819560a4e416520eb1303b76cb37a95fc
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-07-01 18:35:04 +0545

    Worked with Profile page

--- frontend/lib/pages/profile.dart
+++ frontend/lib/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/posts.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -9,9 +10,38 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Profile')),
-      body:Text('Profile')
-    );  }
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.person, size: 80),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("Username", style: TextStyle(fontSize: 18)),
+                      Text("Name", style: TextStyle(color: Colors.grey)),
+                    ],
+                  ),
+                ),
+                Icon(Icons.settings),
+              ],
+            ),
+          ),
+          const Divider(height: 10, thickness: 2, color: Colors.black),
+          Expanded(child: PostsPage()),
+        ],
+      ),
+    );
+  }
 }
