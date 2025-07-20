e77df6552f9b5dd0cab1729483c3a0fec9ca109d
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-07-20 14:12:13 +0545

    Storing, fetching and removing logged in user's user id

--- frontend/lib/pages/Service/authstorage.dart
+++ frontend/lib/pages/Service/authstorage.dart
@@ -1,24 +1,24 @@
+import 'dart:developer';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthStorage {
+  static const _userIdKey = 'userId';
+
   static Future<void> saveUserId(String userId) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('userId', userId);
+    log('Stored user if is $userId');
+    await prefs.setString(_userIdKey, userId);
   }
 
   static Future<String?> getUserId() async {
-    //final prefs = await SharedPreferences.getInstance();
-    //return prefs.getString('userId');
-    return "686b8bc91d177681dc98f1b7";
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_userIdKey);
   }
 
   static Future<void> logout() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.remove('userId');
+    await prefs.remove(_userIdKey);
+    //await prefs.remove(_tokenKey);
   }
 }
-
-/*
-await AuthStorage.saveUserId(userId);
-String? uid = await AuthStorage.getUserId();
-*/
