3a2c366a978cb728a9a6302312d30c0e40ab1cc8
shr-gitt <134399390+shr-gitt@users.noreply.github.com>
2025-07-31 21:22:56 +0545

    Unique Username?

--- Backend/Data/AccountContext.cs
+++ Backend/Data/AccountContext.cs
@@ -6,15 +6,17 @@ namespace Backend.Data;
 
 public class AccountContext
 {
-
     private readonly IMongoDatabase _database;
+    public IMongoCollection<ApplicationUser> Users { get; }
 
-    public AccountContext(IOptions<MongoDbSettings> settings, IMongoClient mongoClient)
+    public AccountContext(IOptions<MongoDbSettings> settings, IMongoClient mongoClient, IConfiguration configuration)
     {
         _database = mongoClient.GetDatabase(settings.Value.DatabaseName);
-    }
-    
-    public IMongoCollection<ApplicationUser> Users =>
-        _database.GetCollection<ApplicationUser>("TouristUsers");
+        Users = _database.GetCollection<ApplicationUser>("TouristUsers");
 
+        var indexKeys = Builders<ApplicationUser>.IndexKeys.Ascending(u => u.NormalizedUserName);
+        var indexOptions = new CreateIndexOptions { Unique = true };
+        var indexModel = new CreateIndexModel<ApplicationUser>(indexKeys, indexOptions);
+        Users.Indexes.CreateOne(indexModel);
+    }
 }
